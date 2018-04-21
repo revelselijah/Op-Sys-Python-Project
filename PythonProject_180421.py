7b27bcf97dba4da9c83c20fa2977937a7e7d52f4
revelselijah <37001213+revelselijah@users.noreply.github.com>
2018-04-21 11:08:12 -0400

    Initial File Created

---
+++ PythonProject_180421.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import tkinter as tk
+from tkinter import messagebox
+from tkinter import filedialog
+import os
+
+
+def main():
+    print("Main starting...")
+    root = Tk()
+    frame = Frame(root)
+
+
+main()
